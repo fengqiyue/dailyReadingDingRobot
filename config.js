fe4ecce629acda14ba47358ee64f7d9d83e65c59
cheri <huanyue.zx@raycloud.com>
2021-05-04 16:12:22 +0800

    feat:增加一个群的机器人

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
-const dLinkList = [`https://oapi.dingtalk.com/robot/send?access_token=a4b40ac9a3339f5aca6551c8e7d30fb81b3749642f5d78c7637f5732355ffd33`];
+const dLinkList = [
+  `https://oapi.dingtalk.com/robot/send?access_token=a4b40ac9a3339f5aca6551c8e7d30fb81b3749642f5d78c7637f5732355ffd33`, // 钉钉功能测试群的机器人
+  `https://oapi.dingtalk.com/robot/send?access_token=2ddfcd46463416993a6d48894a21d8d9db1d34d6158db00797ba18852e844f22`,// 中小前端leader2021 的机器人
+];
 
 exports.dLinkList = dLinkList;
