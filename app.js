0dd8cfb347e00e139a7c85e4eee82481c978328d
cheri <mynameiszhangxu@gmail.com>
2021-05-05 12:11:44 +0800

    修正一处错误
    fix:  遗漏了一个 async

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const getDailyReadingInfo = require('./libs/getDailyReadingInfo.js');
 
 const  scheduleCronstyle = ()=>{
   //每分钟的第30秒定时执行一次:
-    schedule.scheduleJob('8 9 10 * * *',()=>{ //每天的10点9分8秒执行
+    schedule.scheduleJob('8 9 10 * * *', async ()=>{ //每天的10点9分8秒执行
     // schedule.scheduleJob('10 * * * * *', async ()=>{ //每分钟的第十秒执行，测试用
         console.log('scheduleCronstyle:' + new Date());
 
